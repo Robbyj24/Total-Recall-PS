9129964e4a9c3f5a9f5e1e7a83712d4f5e35f39b
Robbyj23 <artcarobert@gmail.com>
2023-12-13 20:07:59 -0500

    Version of a user and admin page using Bootstrap.

---
+++ Section1/src/index.js
@@ -0,0 +1,45 @@
+
+// This is a constant variable called myName 
+const myName = "Gerry Jean"; 
+// this is a variable called favoritelangueage
+let favoriteLanguage ="English";
+// I assign JavaScript as my newFavoritelanguage
+let newFavoriteLanguage = "JavaScript";
+// Now I assign the value of NewFavoritelanguage to my favoritelanguage
+favoriteLanguage = newFavoriteLanguage;
+
+// The final message should show name and newFavorite language
+console.log(`Hello, my name is ${myName} and my favorite programming language is ${favoriteLanguage}.`);
+
+
+
+
+
+// B. STRINGS EXERCISE NOW
+
+
+let firstVariable;
+
+// Assign it the value of the string "Hello World"
+firstVariable = "Hello World";
+
+// Change the value of this variable to some number
+firstVariable = 42;
+
+// Store the value of firstVariable in a new variable called secondVariable
+let secondVariable = firstVariable;
+
+// Change the value of secondVariable to any string
+secondVariable = "I am a string now";
+
+// Log the value of firstVariable
+console.log("The value of firstVariable is:", firstVariable);
+
+// Create a variable called yourName and set it equal to your name as a string
+let yourName = "Your Name"; // Replace "Your Name" with your actual name
+
+// Write an expression that concatenates the strings
+let concatenatedString = "Hello, my name is " + yourName;
+
+// Log the concatenated string
+console.log(concatenatedString);
